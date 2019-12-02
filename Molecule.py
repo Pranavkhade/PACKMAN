c2d8e830488fc756c1c171d8a81923f3503fcee3
Pranav Khade <pranav.m.khade@gmail.com>
2019-12-02 14:24:18 -0600

    After publication

--- Molecule.py
+++ Molecule.py
@@ -82,6 +82,12 @@ class Atom():
         :returns: Element of an 'Atom'
         """
         return self.__Element
+    
+    def get_domain_id(self):
+        """
+        :returns: Domain ID of the parent Residue
+        """
+        return self.get_parent().get_domain_id()
 
     #Set Functions
     def set_id(self,new_id):
@@ -325,6 +331,9 @@ class Residue():
         for i in sorted(self.__Atoms.keys()):yield self.__Atoms[i]
     
     def get_domain_id(self):
+        '''
+        :returns: The assigned domain ID of the residue
+        '''
         return self.__domain_id
     
     #Set Functions
@@ -522,6 +531,12 @@ class Chain():
         """
         for i in sorted(self.__Residues.keys()):yield self.__Residues[i]
     
+    def get_calpha(self):
+        """
+        :returns: Generator yielding all the 'C-Alpha' Atom objects in a Chain.
+        """
+        for i in self.get_residues():yield i.get_calpha()
+    
     def get_hetmols(self):
         """
         :returns: Generator yielding all the 'HetMol' objects in a Chain.
@@ -595,7 +610,7 @@ class Model():
         """
         :returns: Generator yielding all the 'Residue' objects in a Model
         """
-        for i in sorted(self.__AllResidues.keys()):yield self.__AllResidues[i]
+        return [j for i in self.__AllChains.keys() for j in self.__AllChains[i].get_residues()]
     
     def get_atoms(self):
         """
@@ -635,6 +650,87 @@ class Model():
         """
         self.__id=new_id
     
+    def set_torsion(self,Atom1,Atom2,Atom3,Atom4,theta):
+        """
+        NOTE: Currently we can only set torsion for the backbone atoms (Psi and Phi)
+        """
+        radang=self.get_torsion(Atom1,Atom2,Atom3,Atom4)
+        Atom1=self.get_atom_byid(Atom1)
+        Atom2=self.get_atom_byid(Atom2)
+        Atom3=self.get_atom_byid(Atom3)
+        Atom4=self.get_atom_byid(Atom4)
+        torsion_type=Atom1.get_name()+Atom2.get_name()+Atom3.get_name()+Atom4.get_name()
+
+        if(torsion_type=='NCACN'):
+            rotang=numpy.deg2rad(theta)-radang
+        elif(torsion_type=='CNCAC'):
+            rotang=radang-numpy.deg2rad(theta)
+        else:
+            raise Exception('Not a Psi/Phi angle. Current version of PACKMAN only supports Psi/Phi rotation.')
+        
+        sn=numpy.sin(rotang)
+        cs=numpy.cos(rotang)
+        t=1-cs
+
+        v2x=Atom2.get_location()[0]-Atom3.get_location()[0]
+        v2y=Atom2.get_location()[1]-Atom3.get_location()[1]
+        v2z=Atom2.get_location()[2]-Atom3.get_location()[2]
+        #Normalize the rotation vector
+        mag=numpy.sqrt(numpy.square(v2x)+numpy.square(v2y)+numpy.square(v2z))
+        x = float(v2x)/mag
+        y = float(v2y)/mag
+        z = float(v2z)/mag
+        #set up the rotation matrix
+        m=numpy.zeros(9)
+        m[0]= t*x*x + cs
+        m[1]= t*x*y + sn*z
+        m[2]= t*x*z - sn*y
+        m[3]= t*x*y - sn*z
+        m[4]= t*y*y + cs
+        m[5]= t*y*z + sn*x
+        m[6]= t*x*z + sn*y
+        m[7]= t*y*z - sn*x
+        m[8]= t*z*z + cs
+
+        #Rotate The Atoms
+        tx = Atom2.get_location()[0]
+        ty = Atom2.get_location()[1]
+        tz = Atom2.get_location()[2]
+
+        import copy
+        for i in self.get_atoms():
+            Coordinates=i.get_location()
+            #Imporve this later on, this skips some atoms
+            if(torsion_type=='CNCAC' and i.get_parent().get_id()<=Atom1.get_parent().get_id()):
+                Coordinates[0]-=tx
+                Coordinates[1]-=ty
+                Coordinates[2]-=tz
+                x=Coordinates[0]*m[0]+Coordinates[1]*m[1]+Coordinates[2]*m[2]
+                y=Coordinates[0]*m[3]+Coordinates[1]*m[4]+Coordinates[2]*m[5]
+                z=Coordinates[0]*m[6]+Coordinates[1]*m[7]+Coordinates[2]*m[8]
+                Coordinates[0]=x
+                Coordinates[1]=y
+                Coordinates[2]=z
+                Coordinates[0]+=tx
+                Coordinates[1]+=ty
+                Coordinates[2]+=tz
+                i.set_location(numpy.array(Coordinates))
+            if(torsion_type=='NCACN' and i.get_parent().get_id()>=Atom4.get_parent().get_id()):
+                Coordinates[0]-=tx
+                Coordinates[1]-=ty
+                Coordinates[2]-=tz
+                x=Coordinates[0]*m[0]+Coordinates[1]*m[1]+Coordinates[2]*m[2]
+                y=Coordinates[0]*m[3]+Coordinates[1]*m[4]+Coordinates[2]*m[5]
+                z=Coordinates[0]*m[6]+Coordinates[1]*m[7]+Coordinates[2]*m[8]
+                Coordinates[0]=x
+                Coordinates[1]=y
+                Coordinates[2]=z
+                Coordinates[0]+=tx
+                Coordinates[1]+=ty
+                Coordinates[2]+=tz
+                i.set_location(numpy.array(Coordinates))
+        return True
+    
     #Compute Functions
     def get_calpha(self):
         """
@@ -647,6 +743,28 @@ class Model():
         :returns: All 'Backbone' atoms as an 'Atom object.'
         """
         return [i.get_backbone() for i in self.get_residues()]
+    
+    def get_torsion(self,Atom1,Atom2,Atom3,Atom4):
+        """
+        """
+        Atom1=self.get_atom_byid(Atom1)
+        Atom2=self.get_atom_byid(Atom2)
+        Atom3=self.get_atom_byid(Atom3)
+        Atom4=self.get_atom_byid(Atom4)
+        p0=Atom1.get_location()
+        p1=Atom2.get_location()
+        p2=Atom3.get_location()
+        p3=Atom4.get_location()
+        b0 = -1.0*(p1 - p0)
+        b1 = p2 - p1
+        b2 = p3 - p2
+        b1 /= numpy.linalg.norm(b1)
+        v = b0 - numpy.dot(b0, b1)*b1
+        w = b2 - numpy.dot(b2, b1)*b1
+        x = numpy.dot(v, w)
+        y = numpy.dot(numpy.cross(b1, v), w)
+        radang=numpy.arctan2(y, x)
+        return radang
     
     #Check Function
     def check_clashes(self,distance=0.77):
@@ -664,6 +782,17 @@ class Protein():
     
     def __getitem__(self,ModelNumber):
         return self.Models[ModelNumber]
+    
+    def write_pdb(self,filename):
+        #sys.stdout.write("%-6s %-50s %-25s\n" % (code, name, industry))
+        open(filename,'w').write('')
+        fh=open(filename,'a')
+        for num_,_ in enumerate(self):
+            fh.write("Model\t"+str(num_)+'\n')
+            for i in _.get_atoms():
+                fh.write("ATOM  %5s %-4s %3s %1s%4s    %8s%8s%8s%6s%6s         %-4s%2s%2s\n"%(i.get_id(),i.get_name(),i.get_parent().get_name(),i.get_parent().get_parent().get_id(),i.get_parent().get_id(),numpy.around(i.get_location()[0],decimals=3),numpy.around(i.get_location()[1],decimals=3),numpy.around(i.get_location()[2],decimals=3),i.get_occupancy(),i.get_bfactor(),'',i.get_element(),''))
+            fh.write("ENDMDL\n")
+        return True
 
 '''
 ##################################################################################################
@@ -824,21 +953,11 @@ def DownloadPDB(pdbid,save_name='Download.pdb'):
     import urllib.request as ur
     response=ur.urlopen('https://files.rcsb.org/view/'+pdbid+'.pdb')
     try:
-        open(save_name,'w').write(response.read())
+        open(save_name,'wb').write(response.read())
     except(IOError):
         None
     return True
 
-def WritePDB(molecule,filename):
-    #sys.stdout.write("%-6s %-50s %-25s\n" % (code, name, industry))
-    open(filename,'w').write('')
-    fh=open(filename,'a')
-    for num_,_ in enumerate(molecule):
-        fh.write("Model\t"+str(num_)+'\n')
-        for i in _.get_atoms():
-            fh.write("ATOM  %5s %-4s %3s %1s%4s    %8s%8s%8s%6s%6s         %-4s%2s%2s\n"%(i.get_id(),i.get_name(),i.get_parent().get_name(),i.get_parent().get_parent().get_id(),i.get_parent().get_id(),i.get_location()[0],i.get_location()[1],i.get_location()[2],i.get_occupancy(),i.get_bfactor(),'',i.get_element(),''))
-        fh.write("ENDMDL\n")
-    return True
 
 '''
 ##################################################################################################
