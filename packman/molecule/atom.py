c800128d43362fa964770db5980f71ed0b087c94
Pranav Khade <pranav.m.khade@gmail.com>
2020-02-11 17:51:12 -0600

    compliance added.

--- packman/molecule/atom.py
+++ packman/molecule/atom.py
@@ -6,8 +6,15 @@ import numpy
 
 
 class Atom():
-    """
-    This class contains the information about the Atom.
+    """This class contains the information about the Atom.
+
+    This class contains all the information available about the Atom. The Atom class is the lowest in the hierarchy of the 'molecule' API classes.
+    the order of hierarchy being: Protein> Model> Chain> Residue> Atom. This class is also the component of the 'molecule' module API.
+    Please read the Tutorials and Documentation for more details.
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
     """
     def __init__(self,id,AtomName,Coordinates,Occupancy,bfactor,Element,Charge,parent):
         self.__id=id
@@ -21,60 +28,70 @@ class Atom():
         self.__Element=Element
 
     #Get Functions
+    @property
     def get_id(self):
         """
-        :returns: ID of an 'Atom' object
+        :returns: ID of an 'packman.molecule.atom.Atom' object
         """
         return self.__id
     
+    @property
     def get_name(self):
         """
         :returns: Name of an 'Atom' object
         """
         return self.__AtomName
     
+    @property
     def get_alternatelocationindicator(self):
         """
         :returns: Alternate Location Indicator
         """
         return self.__AlternateLocationIndicator
 
+    @property
     def get_parent(self):
         """
         :returns: Parent residue of an 'Atom' Object. (If available)
         """
         return self.__parent
     
+    @property
     def get_location(self):
         """
         :returns: Cartesian Coordinates of an 'Atom'
         """
         return self.__Coordinates
     
+    @property
     def get_occupancy(self):
         """
         :returns: Occupancy of an 'Atom'
         """
         return self.__Occupancy
     
+    @property
     def get_bfactor(self):
         """
         :returns: B-Factor value of an 'Atom'
         """
         return self.__bfactor
     
+    @property
     def get_segmentidentifier(self):
         """
         :returns: Segment Identifier of an 'Atom'
         """
         return self.__SegmentIdentifier
     
+    @property
     def get_element(self):
         """
         :returns: Element of an 'Atom'
         """
         return self.__Element
     
+    @property
     def get_domain_id(self):
         """
         :returns: Domain ID of the parent Residue
