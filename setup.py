3be451befd08d3d06fb5c76eebd4bac2c23d3b4f
Pranav Khade <pranav.m.khade@gmail.com>
2020-02-11 18:39:37 -0600

    minor fixes

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ SCRIPTS=['packman=packman.bin.PACKMAN:main']
 
 
 setup(name='py-packman',
-      version='1.1.0',
+      version='1.1.1',
       description='A software package for molecular PACKing and Motion ANalysis (PACKMAN)',
       url='https://github.com/Pranavkhade/PACKMAN',
       author='Pranav Khade',
