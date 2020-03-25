e9c605e055e1d6a73b58d9046796a08173a88924
Pranav Khade <pranav@Pranavs-MacBook-Pro.local>
2020-03-24 19:20:40 -0500

    annotations1

--- packman/molecule/atom.py
+++ packman/molecule/atom.py
@@ -1,19 +1,50 @@
-'''
-Author: Pranav Khade(pranavk@iastate.edu)
-'''
+# -*- coding: utf-8 -*-
+"""The 'Atom' object host file.
+
+This is file information, not the class information. This information is only for the API developers.
+Please read the 'Atom' object documentation for details. [ help(packman.molecule.atoms.Atom) ]
+
+Example:
+    >>>from packman.molecule import atom.Atom
+    >>>help( Atom )
+    OR
+
+    >>>from packman import molecule
+    >>>help( molecule.Atom )
+
+Todo:
+    * Finish writing up the documentation.
+    * Finish error handling.
+    * Finish optimizing the performance.
+
+Authors:
+    * Pranav Khade(https://github.com/Pranavkhade)
+"""
 
 import numpy
 
 
 class Atom():
-    """This class contains the information about the Atom.
+    """This class contains the information about the 'Atom' object (packman.molecule.Atom).
 
     This class contains all the information available about the Atom. The Atom class is the lowest in the hierarchy of the 'molecule' API classes.
     the order of hierarchy being: Protein> Model> Chain> Residue> Atom. This class is also the component of the 'molecule' module API.
     Please read the Tutorials and Documentation for more details.
 
-    Properties created with the ``@property`` decorator should be documented
-    in the property's getter method.
+    Note:
+        - Most essential class of all as it stores the actual data of the atoms.
+        - Please refer to the [https://web.archive.org/web/20080905024351/http://www.wwpdb.org/docs.html] for the description of the arguments.
+    
+    Args:
+        id (int): Atom ID from the PDB file as it is. Each Atom in a PDB file Model/Frame has unique ID. (essential)
+        AtomName (str): Atom Name from the PDB file as it is.
+        Coordinates (numpy.array): The 3*1 array of coordinates/location of the atoms. (essential)
+        Occupancy (float): The value of the occupancy.
+        bfactor (float): The B-factor/Temperature Factor/ Debye Waller Factor of the atom. (For NMR structures, it is RMSF accross the frames)
+        Element (str): Element information of the given 'Atom' object. (Element from the Periodic Table)
+        Charge (float): Charnge of the given 'Atom' object.
+        parent (packman.molecule.Residue): The Residue Object (parent) this atom belongs to.
+        AlternateLocationIndicator (bool): If the alternate location available for the atom (To be removed in future)
 
     """
     def __init__(self,id,AtomName,Coordinates,Occupancy,bfactor,Element,Charge,parent):
@@ -29,123 +60,174 @@ class Atom():
 
     #Get Functions
     def get_id(self):
-        """
-        :returns: ID of an 'packman.molecule.atom.Atom' object
+        """Get the ID of the 'Atom'
+
+        Returns:
+            int if successful, None otherwise.
         """
         return self.__id
     
     def get_name(self):
-        """
-        :returns: Name of an 'Atom' object
+        """Get the Name of the 'Atom'
+
+        Returns:
+            str if successful, None otherwise.
         """
         return self.__AtomName
     
     def get_alternatelocationindicator(self):
-        """
-        :returns: Alternate Location Indicator
+        """Know if the given 'Atom' has Alternate Location Information
+
+        Note:
+            This feature might be removed in the future.
+
+        Returns:
+            True if successful, False otherwise.
         """
         return self.__AlternateLocationIndicator
 
     def get_parent(self):
-        """
-        :returns: Parent residue of an 'Atom' Object. (If available)
+        """Get the 'Residue' the given 'Atom' belongs to.
+
+        Returns:
+            packman.molecule.Residue if successful, None otherwise.
         """
         return self.__parent
     
     def get_location(self):
-        """
-        :returns: Cartesian Coordinates of an 'Atom'
+        """Get the Coordinates/Location of the given 'Atom'
+
+        Returns:
+            numpy.array if successful, None otherwise.
         """
         return self.__Coordinates
     
     def get_occupancy(self):
-        """
-        :returns: Occupancy of an 'Atom'
+        """Get the Occupancy of the given 'Atom'
+
+        Returns:
+            float if successful, None otherwise.
         """
         return self.__Occupancy
     
     def get_bfactor(self):
-        """
-        :returns: B-Factor value of an 'Atom'
+        """Get the B-factor/Temperature Factor/ Debye Waller Factor of the given 'Atom'
+
+        Returns:
+            float if successful, None otherwise.
         """
         return self.__bfactor
     
     def get_segmentidentifier(self):
-        """
-        :returns: Segment Identifier of an 'Atom'
+        """Get the Segment Identifier of the given 'Atom'
+
+        Returns:
+            str if successful, None otherwise.
         """
         return self.__SegmentIdentifier
     
     def get_element(self):
-        """
-        :returns: Element of an 'Atom'
+        """Get the Element of the given 'Atom'
+
+        Returns:
+            str if successful, None otherwise.
         """
         return self.__Element
     
     def get_domain_id(self):
-        """
-        :returns: Domain ID of the parent Residue
+        """Get the Domain Identifier of the given 'Atom'. Hinge Prediction is Necessary for this option.
+
+        Note:
+            Domain Identifiers (Obtained after running PACKMAN):
+            'FL': Flexible Linkers (Hinges); Followed by the domain number
+            'DM': Domain; Followed by the domain number
+
+        Returns:
+            str if successful, None otherwise.
         """
         return self.get_parent().get_domain_id()
 
     #Set Functions
     def set_id(self,new_id):
-        """
-        :param new_id: New ID to be assigned
+        """Set the ID of the given 'Atom'
+
+        Args:
+            new_id (int): The ID User wishes to assign to the given 'Atom'
         """
         self.__id=new_id
     
     def set_name(self,new_name):
+        """Set the Name of the given 'Atom'
+
+        Args:
+            new_name (str): The Name User wishes to assign to the given 'Atom'
         """
-        :param new_name: New name to be assigned
-        """
-        self.__AtomName=new_name
+        self.__AtomName=new_name    
     
     def set_alternatelocationindicator(self,new_alternatelocationindicator):
-        """
-        :param new_alternatelocationindicator: New Alternate Location Indicator to be assigned
+        """Set the Alternate Location Indicator of the given 'Atom'
+
+        Args:
+            new_alternatelocationindicator: The Alternate Location Indicator User wishes to assign to the given 'Atom'
         """
         self.__AlternateLocationIndicator=new_alternatelocationindicator
 
     def set_parent(self,new_parent):
-        """
-        :param new_parent: New parent to be assigned
+        """Set the Parent 'Residue' of the given 'Atom'
+
+        Args:
+            new_parent (packman.molecule.residue.Residue): The parent 'Residue' User wishes to assign to the given 'Atom'
         """
         self.__parent=new_parent
     
     def set_location(self,new_location):
-        """
-        :param new_location: New Cartesian Coordinates to be assigned
+        """Set the Coordinates/Location of the given 'Atom'
+
+        Args:
+            new_location (numpy.array): The Coordinates/Location User wishes to assign to the given 'Atom'
         """
         self.__Coordinates=new_location
     
     def set_occupancy(self,new_occupancy):
-        """
-        :param new_occupancy: New occupancy value to be assigned
+        """Set the Occupancy of the given 'Atom'
+
+        Args:
+            new_occupancy: The Occupancy User wishes to assign to the given 'Atom'
         """
         self.__Occupancy=new_occupancy
     
     def set_bfactor(self,new_bfactor):
-        """
-        :param new_bfactor: New B-Factor to be assigned
+        """Set the B-factor/Temperature Factor/ Debye Waller Factor of the given 'Atom'
+
+        Args:
+            new_bfactor (float): The B-factor/Temperature Factor/ Debye Waller Factor User wishes to assign to the given 'Atom'
         """
         self.__bfactor=new_bfactor
     
     def set_segmentidentifier(self,new_segmentidentifier):
-        """
-        :param new_segmentidentifier: New Segment Identifier to be assigned
+        """Set the Segment Identifier of the given 'Atom'
+
+        Args:
+            new_segmentidentifier (str): The Segment Identifier User wishes to assign to the given 'Atom'
         """
         self.__SegmentIdentifier=new_segmentidentifier
     
     def set_elment(self,new_element):
-        """
-        :param new_element: New element to be assigned
+        """Set the Element of the given 'Atom'
+
+        Args:
+            new_element (str): The Element User wishes to assign to the given 'Atom'
         """
         self.__Element=new_element
 
     #Calculation Functions
     def calc_dist(self,another_atom):
-        """
-        :param another_atom: 'Atom' object to which the distance should be calculated.
+        """Calculate the Distance between the given 'Atom' and another 'Atom'
+
+        Args:
+            another_atom (packman.molecule.atom.Atom): The 'Atom' User wishes to calculate distance from.
+        
+        Returns:
+            float if successful, None otherwise.
         """
         return numpy.linalg.norm(self.__Coordinates-another_atom.get_location())
--- packman/molecule/residue.py
+++ packman/molecule/residue.py
@@ -1,16 +1,45 @@
-'''
-Author: Pranav Khade(pranavk@iastate.edu)
-'''
+# -*- coding: utf-8 -*-
+"""The 'Residue' object host file.
+
+This is file information, not the class information. This information is only for the API developers.
+Please read the 'Res' object documentation for details. [ help(packman.molecule.atoms.Atom) ]
+
+Example:
+    >>>from packman.molecule import residue.Residue
+    >>>help( Residue )
+    OR
+
+    >>>from packman import molecule
+    >>>help( molecule.Residue )
+
+Todo:
+    * Finish writing up the documentation.
+    * Finish error handling.
+    * Finish optimizing the performance.
+
+Authors:
+    * Pranav Khade(https://github.com/Pranavkhade)
+"""
 
 import numpy
 
 
 class Residue():
-    '''
-    Domain Identifiers: (Obtained after running PACKMAN)
-    FL: Flexible Linkers (Hinges); Followed by a number
-    DM: Domain; Followed by a number
-    '''
+    """This class contains the information about the 'Residue' object (packman.molecule.Residue).
+
+    This class contains all the information available about the Residue and stores the corresponding 'Atom' objects in itself. The Residue class is the second lowest in the hierarchy of the 'molecule' API classes.
+    the order of hierarchy being: Protein> Model> Chain> Residue> Atom. This class is also the component of the 'molecule' module API.
+    Please read the Tutorials and Documentation for more details.
+
+    Note:
+        - Please refer to the [https://web.archive.org/web/20080905024351/http://www.wwpdb.org/docs.html] for the description of the arguments.
+    
+    Args:
+        id (int): Residue ID from the PDB file as it is. Each Residue in a PDB file Model/Frame has unique ID. (essential)
+        name (str): Residue Name from the PDB file as it is.
+        parent (packman.molecule.Chain): The Chain Object (parent) this Residue belongs to.
+
+    """
     def __init__(self,id,name,parent):
         self.__id=id
         self.__name=name
@@ -19,6 +48,11 @@ class Residue():
         self.__domain_id=None
     
     def __setitem__(self,id,Atom):
+        """Simply assign new atom to the 'Residue'
+
+        Args:
+            new_id: The ID User wishes to assign to the given 'Atom'
+        """
         try:
             self.__Atoms[id]=Atom
         except:
@@ -27,68 +61,105 @@ class Residue():
     
     #Get Functions
     def get_id(self):
-        """
-        :returns: ID of an 'Residue' object
+        """Get the ID of the 'Residue'
+
+        Returns:
+            int if successful, None otherwise.
         """
         return self.__id
     
     def get_name(self):
-        """
-        :returns: Name of an 'Residue' object
+        """Get the Name of the 'Residue'
+
+        Returns:
+            str if successful, None otherwise.
         """
         return self.__name
 
     def get_parent(self):
-        """
-        :returns: Parent residue of an 'Residue' Object. (If available)
+        """Get the Parent of the 'Residue'
+
+        Returns:
+            packman.molecule.Chain if successful, None otherwise.
         """
         return self.__parent
     
     def get_atoms(self):
-        """
-        :returns: Generator yielding all the 'Atom' objects in a residue.
+        """Get the generator of corresponding 'Atom' objects of the 'Residue'
+
+        Returns:
+            generator of 'Atom' objects if successful, None otherwise.
         """
         for i in sorted(self.__Atoms.keys()):yield self.__Atoms[i]
     
     def get_domain_id(self):
-        '''
-        :returns: The assigned domain ID of the residue
-        '''
+        """Get the Domain Identifier of the given 'Residue'. Hinge Prediction is Necessary for this option.
+
+        Note:
+            Domain Identifiers (Obtained after running PACKMAN):
+            'FL': Flexible Linkers (Hinges); Followed by the domain number
+            'DM': Domain; Followed by the domain number
+
+        Returns:
+            str if successful, None otherwise.
+        """
         return self.__domain_id
     
     #Set Functions
     def set_id(self,new_id):
-        """
-        :param new_id: New ID to be assigned
+        """Set the ID of the given 'Residue'
+
+        Args:
+            new_id (int): The ID User wishes to assign to the given 'Residue'
         """
         self.__id=new_id
     
     def set_name(self,new_name):
-        """
-        :param new_name: New name to be assigned
+        """Set the Name of the given 'Residue'
+
+        Args:
+            new_name (str): The Name User wishes to assign to the given 'Residue'
         """
         self.__name=new_name
     
     def set_parent(self,new_parent):
-        """
-        :param new_parent: New parent to be assigned
+        """Set the Parent of the given 'Residue'
+
+        Args:
+            new_parent (packman.molecule.Chain): The parent 'Chain' User wishes to assign to the given 'Residue'
         """
         self.__parent=new_parent
 
     def set_domain_id(self,new_domain_id):
+        """Set the Domain Identifier of the given 'Residue'.
+
+        Note:
+            Domain Identifiers Format:
+            'FL': Flexible Linkers (Hinges); Followed by the domain number
+            'DM': Domain; Followed by the domain number
+
+        Args:
+            new_domain_id (str): The Domain Identifies User wishes to assign to the given 'Residue'
+        """
         self.__domain_id=new_domain_id
 
     #Calculation Functions
     def get_calpha(self):
-        """
-        :returns: A 'C-Alpha' atom as an 'Atom' object.
+        """Get the C-Alpha atom of the residue as an 'Atom' object.
+
+        Returns:
+            packman.molecule.Atom if successful, None otherwise.
         """
         return [i for i in self.get_atoms() if i.get_name()=='CA'][0]
     
     def get_centerofgravity(self):
-        """
-        NOTE: Yet to add the atomic masses.
-        :returns: Cartesian Coordinates of the centre of the gravity.
+        """Get the center of gravity of the given 'Residue'
+
+        Note:
+            Yet to add the atomic masses.
+
+        Returns:
+            Cartesian Coordinates as numpy.array of the centre of the gravity.
         """
         atoms=self.get_atoms()
         AtomicMass=1
@@ -102,15 +173,24 @@ class Residue():
         return numpy.array([i/MassofAA for i in XYZ_M])
     
     def get_backbone(self):
-        """
-        NOTE: Some files like 1k20 are showing multiple backbone atoms because seperation of models is based on [Model] in this tool
-        :returns: 'Backbone' atoms as an 'Atom' object.
+        """Get the Backbone atoms of the given 'Residue' as a list of 'Atom' object
+
+        Note:
+            Some files like 1k20 are showing multiple backbone atoms because seperation of models is based on [Model] in this tool (Maybe Solved)
+
+        Returns:
+            list of packman.molecule.Atom if successful, None otherwise.
         """
         return [i for i in self.get_atoms() if(i.get_name()=='N' or i.get_name()=='CA' or i.get_name()=='C' or i.get_name()=='O')]
     
     def get_tip(self):
-        """
-        :returns: Tip atom of an amino acid as an 'Atom' object.
+        """Get the tip atom of the given 'Residue' as an 'Atom' object
+
+        Note:
+            ALA and GLY are small so their tip is C-alpha
+
+        Returns:
+            packman.molecule.Atom if successful, None otherwise.
         """
         CAlpha=self.get_calpha()
         resname=self.get_name()
