054d84a4c5d64978e94df0e8686634a2514868a9
Pranav Khade <pranav@Pranavs-MacBook-Pro.local>
2020-03-26 22:08:07 -0500

    annotations3; HetAtom changed

--- packman/molecule/hetatom.py
+++ packman/molecule/hetatom.py
@@ -1,137 +1,165 @@
-'''
-Author: Pranav Khade(pranavk@iastate.edu)
-'''
+# -*- coding: utf-8 -*-
+"""The 'HetAtom' object host file.
+
+This is file information, not the class information. This information is only for the API developers.
+Please read the 'HetAtom' object documentation for details. [ help(packman.molecule.residue.Residue) ]
+
+Example:
+    >>>from packman.molecule import residue.HetAtom
+    >>>help( HetAtom )
+    OR
+
+    >>>from packman import HetAtom
+    >>>help( molecule.HetAtom )
+
+Todo:
+    * Finish writing up the documentation.
+    * Finish error handling.
+    * Finish optimizing the performance.
+
+Authors:
+    * Pranav Khade(https://github.com/Pranavkhade)
+"""
 
 import numpy
 
-class HetAtom():
-    """
-    This class contains the information about the HetAtom.
-    """
-    def __init__(self,id,AtomName,Coordinates,Occupancy,bfactor,Element,Charge,parent):
-        self.__id=id
-        self.__AtomName=AtomName
-        self.__AlternateLocationIndicator=None #remove it later
-        self.__parent=parent
-        self.__Coordinates=Coordinates
-        self.__Occupancy=Occupancy
-        self.__bfactor=bfactor
-        self.__SegmentIdentifier=None
-        self.__Element=Element
 
+class HetAtom():
+    """This class contains the information about the 'HetAtom' object (packman.molecule.Residue).
+
+    This class contains all the information available about the HetAtom and stores the corresponding 'Atom' objects in itself. The Residue class is the second lowest in the hierarchy of the 'molecule' API classes.
+    the order of hierarchy being: Protein> Model> Chain> HetAtom> Atom. This class is also the component of the 'molecule' module API.
+    Please read the Tutorials and Documentation for more details.
+
+    Note:
+        - Please refer to the [https://web.archive.org/web/20080905024351/http://www.wwpdb.org/docs.html] for the description of the arguments.
+    
+    Args:
+        id (int): Residue ID from the PDB file as it is. Each Residue in a PDB file Model/Frame has unique ID. (essential)
+        name (str): Residue Name from the PDB file as it is.
+        parent (packman.molecule.Chain): The Chain Object (parent) this Residue belongs to.
+
+    """
+    def __init__(self,id,name,parent):
+        self.__id=id
+        self.__name=name
+        self.__parent=parent
+        self.__Atoms=None
+        self.__domain_id=None
+    
+    def __setitem__(self,id,Atom):
+        """Simply assign new atom to the 'HetAtom'
+
+        Args:
+            new_id: The ID User wishes to assign to the given 'Atom'
+        """
+        try:
+            self.__Atoms[id]=Atom
+        except:
+            self.__Atoms={}
+            self.__Atoms[id]=Atom
+    
     #Get Functions
     def get_id(self):
-        """
-        :returns: ID of an 'Atom' object
+        """Get the ID of the 'HetAtom'
+
+        Returns:
+            int if successful, None otherwise.
         """
         return self.__id
     
     def get_name(self):
+        """Get the Name of the 'HetAtom'
+
+        Returns:
+            str if successful, None otherwise.
         """
-        :returns: Name of an 'Atom' object
-        """
-        return self.__AtomName
-    
-    def get_alternatelocationindicator(self):
-        """
-        :returns: Alternate Location Indicator
-        """
-        return self.__AlternateLocationIndicator
+        return self.__name
 
     def get_parent(self):
-        """
-        :returns: Parent residue of an 'Atom' Object. (If available)
+        """Get the Parent of the 'HetAtom'
+
+        Returns:
+            packman.molecule.Chain if successful, None otherwise.
         """
         return self.__parent
     
-    def get_location(self):
-        """
-        :returns: Cartesian Coordinates of an 'Atom'
-        """
-        return self.__Coordinates
-    
-    def get_occupancy(self):
-        """
-        :returns: Occupancy of an 'Atom'
-        """
-        return self.__Occupancy
-    
-    def get_bfactor(self):
-        """
-        :returns: B-Factor value of an 'Atom'
-        """
-        return self.__bfactor
-    
-    def get_segmentidentifier(self):
-        """
-        :returns: Segment Identifier of an 'Atom'
-        """
-        return self.__SegmentIdentifier
-    
-    def get_elment(self):
-        """
-        :returns: Element of an 'Atom'
-        """
-        return self.__Element
+    def get_atoms(self):
+        """Get the generator of corresponding 'Atom' objects of the 'HetAtom'
 
+        Returns:
+            generator of 'Atom' objects if successful, None otherwise.
+        """
+        for i in sorted(self.__Atoms.keys()):yield self.__Atoms[i]
+    
+    def get_domain_id(self):
+        """Get the Domain Identifier of the given 'HetAtom'. Hinge Prediction is Necessary for this option.
+
+        Note:
+            Domain Identifiers (Obtained after running PACKMAN):
+            'FL': Flexible Linkers (Hinges); Followed by the domain number
+            'DM': Domain; Followed by the domain number
+
+        Returns:
+            str if successful, None otherwise.
+        """
+        return self.__domain_id
+    
     #Set Functions
     def set_id(self,new_id):
-        """
-        :param new_id: New ID to be assigned
+        """Set the ID of the given 'HetAtom'
+
+        Args:
+            new_id (int): The ID User wishes to assign to the given 'HetAtom'
         """
         self.__id=new_id
     
     def set_name(self,new_name):
-        """
-        :param new_name: New name to be assigned
-        """
-        self.__AtomName=new_name
-    
-    def set_alternatelocationindicator(self,new_alternatelocationindicator):
-        """
-        :param new_alternatelocationindicator: New Alternate Location Indicator to be assigned
-        """
-        self.__AlternateLocationIndicator=new_alternatelocationindicator
+        """Set the Name of the given 'HetAtom'
 
-    def set_parent(self,new_parent):
+        Args:
+            new_name (str): The Name User wishes to assign to the given 'HetAtom'
         """
-        :param new_parent: New parent to be assigned
+        self.__name=new_name
+    
+    def set_parent(self,new_parent):
+        """Set the Parent of the given 'HetAtom'
+
+        Args:
+            new_parent (packman.molecule.Chain): The parent 'Chain' User wishes to assign to the given 'HetAtom'
         """
         self.__parent=new_parent
-    
-    def set_location(self,new_location):
+
+    def set_domain_id(self,new_domain_id):
+        """Set the Domain Identifier of the given 'HetAtom'.
+
+        Note:
+            Domain Identifiers Format:
+            'FL': Flexible Linkers (Hinges); Followed by the domain number
+            'DM': Domain; Followed by the domain number
+
+        Args:
+            new_domain_id (str): The Domain Identifies User wishes to assign to the given 'HetAtom'
         """
-        :param new_location: New Cartesian Coordinates to be assigned
-        """
-        self.__Coordinates=new_location
-    
-    def set_occupancy(self,new_occupancy):
-        """
-        :param new_occupancy: New occupancy value to be assigned
-        """
-        self.__Occupancy=new_occupancy
-    
-    def set_bfactor(self,new_bfactor):
-        """
-        :param new_bfactor: New B-Factor to be assigned
-        """
-        self.__bfactor=new_bfactor
-    
-    def set_segmentidentifier(self,new_segmentidentifier):
-        """
-        :param new_segmentidentifier: New Segment Identifier to be assigned
-        """
-        self.__SegmentIdentifier=new_segmentidentifier
-    
-    def set_elment(self,new_element):
-        """
-        :param new_element: New element to be assigned
-        """
-        self.__Element=new_element
+        self.__domain_id=new_domain_id
 
     #Calculation Functions
-    def calc_dist(self,another_atom):
+    def get_centerofgravity(self):
+        """Get the center of gravity of the given 'HetAtom'
+
+        Note:
+            Yet to add the atomic masses.
+
+        Returns:
+            Cartesian Coordinates as numpy.array of the centre of the gravity.
         """
-        :param another_atom: 'Atom' object to which the distance should be calculated.
-        """
-        return numpy.linalg.norm(self.__Coordinates-another_atom.get_location())
+        atoms=self.get_atoms()
+        AtomicMass=1
+        XYZ_M=[0,0,0]
+        MassofAA=0
+        for i in atoms:
+            XYZ_M[0]+=i.Coordinates[0]*AtomicMass
+            XYZ_M[1]+=i.Coordinates[1]*AtomicMass
+            XYZ_M[2]+=i.Coordinates[2]*AtomicMass
+            MassofAA=MassofAA+AtomicMass
+        return numpy.array([i/MassofAA for i in XYZ_M])
--- packman/molecule/model.py
+++ packman/molecule/model.py
@@ -109,27 +109,100 @@ class Model():
         return self.__AllAtoms[query_atom_id]
     
     def get_hetmols(self):
-        """
-        :returns: Generator yielding all the 'HetMol' objects in a Model
+        """Get the generator of corresponding 'HetMol' objects of the 'Model'
+
+        Returns:
+            generator of 'packman.molecule.HetMol' objects if successful, None otherwise.
         """
         for i in sorted(self.__AllHetMols.keys()):yield self.__AllHetMols[i]
      
     def get_hetatoms(self):
-        """
-        :returns: Generator yielding all the 'HetAtom' objects in a Model
+        """Get the generator of corresponding 'HetAtom' objects of the 'Model'
+
+        Returns:
+            generator of 'packman.molecule.HetAtom' objects if successful, None otherwise.
         """
         for i in sorted(self.__AllHetAtoms.keys()):yield self.__AllHetAtoms[i]
-    
-    #Set Functions
-    def set_id(self,new_id):
+        
+    #Compute Functions
+    def get_calpha(self):
+        """Get the C-Alpha atom of the 'Model' as an 'Atom' object.
+
+        Returns:
+            list of packman.molecule.Atom if successful, None otherwise.
         """
-        :param new_id: New ID to be assigned
+        return [i.get_calpha() for i in self.get_residues()]
+    
+    def get_backbone(self):
+        """Get the Backbone atoms of the given 'Model' as a list of 'Atom' object
+
+        Note:
+            * Backbone Atoms: CA, O, N, C
+
+        Returns:
+            list of packman.molecule.Atom if successful, None otherwise.
+        """
+        return [i.get_backbone() for i in self.get_residues()]
+    
+    def get_torsion(self,Atom1,Atom2,Atom3,Atom4):
+        """Get the torsion angle between the two planes defined by four atoms.
+
+        Notes:
+            * This function will be moved to the molecule manipulation package later
+
+        
+        Args:
+            Atom1 (packman.molecule.Atom): First reference 'Atom'
+            Atom2 (packman.molecule.Atom): Second reference 'Atom'
+            Atom3 (packman.molecule.Atom): Third reference 'Atom'
+            Atom4 (packman.molecule.Atom): Fourth reference 'Atom'
+        
+        Returns:
+            theta (float)                : Angle user wishes to set (radian)
+        
+        """
+        Atom1=self.get_atom_byid(Atom1)
+        Atom2=self.get_atom_byid(Atom2)
+        Atom3=self.get_atom_byid(Atom3)
+        Atom4=self.get_atom_byid(Atom4)
+        p0=Atom1.get_location()
+        p1=Atom2.get_location()
+        p2=Atom3.get_location()
+        p3=Atom4.get_location()
+        b0 = -1.0*(p1 - p0)
+        b1 = p2 - p1
+        b2 = p3 - p2
+        b1 /= numpy.linalg.norm(b1)
+        v = b0 - numpy.dot(b0, b1)*b1
+        w = b2 - numpy.dot(b2, b1)*b1
+        x = numpy.dot(v, w)
+        y = numpy.dot(numpy.cross(b1, v), w)
+        radang=numpy.arctan2(y, x)
+        return radang
+    
+        #Set Functions
+    def set_id(self,new_id):
+        """Set the ID of the given 'Model'
+
+        Args:
+            new_id (int): The ID User wishes to assign to the given 'Model'
         """
         self.__id=new_id
     
     def set_torsion(self,Atom1,Atom2,Atom3,Atom4,theta):
-        """
-        NOTE: Currently we can only set torsion for the backbone atoms (Psi and Phi)
+        """Set the torsion angle between the two planes defined by four atoms.
+
+        Notes:
+            * This function will be moved to the molecule manipulation package later
+
+        
+        Args:
+            Atom1 (packman.molecule.Atom): First reference 'Atom'
+            Atom2 (packman.molecule.Atom): Second reference 'Atom'
+            Atom3 (packman.molecule.Atom): Third reference 'Atom'
+            Atom4 (packman.molecule.Atom): Fourth reference 'Atom'
+            theta (float)                : Angle user wishes to set (Degree)
+        
         """
         radang=self.get_torsion(Atom1,Atom2,Atom3,Atom4)
         Atom1=self.get_atom_byid(Atom1)
@@ -207,44 +280,21 @@ class Model():
                 Coordinates[2]+=tz
                 i.set_location(numpy.array(Coordinates))
         return True
-    
-    #Compute Functions
-    def get_calpha(self):
-        """
-        :returns: All 'C-Alpha' atoms as an 'Atom' object.
-        """
-        return [i.get_calpha() for i in self.get_residues()]
-    
-    def get_backbone(self):
-        """
-        :returns: All 'Backbone' atoms as an 'Atom object.'
-        """
-        return [i.get_backbone() for i in self.get_residues()]
-    
-    def get_torsion(self,Atom1,Atom2,Atom3,Atom4):
-        """
-        """
-        Atom1=self.get_atom_byid(Atom1)
-        Atom2=self.get_atom_byid(Atom2)
-        Atom3=self.get_atom_byid(Atom3)
-        Atom4=self.get_atom_byid(Atom4)
-        p0=Atom1.get_location()
-        p1=Atom2.get_location()
-        p2=Atom3.get_location()
-        p3=Atom4.get_location()
-        b0 = -1.0*(p1 - p0)
-        b1 = p2 - p1
-        b2 = p3 - p2
-        b1 /= numpy.linalg.norm(b1)
-        v = b0 - numpy.dot(b0, b1)*b1
-        w = b2 - numpy.dot(b2, b1)*b1
-        x = numpy.dot(v, w)
-        y = numpy.dot(numpy.cross(b1, v), w)
-        radang=numpy.arctan2(y, x)
-        return radang
-    
+
     #Check Function
     def check_clashes(self,distance=0.77):
+        """Check if any atoms are too close to each other. This is important since too close atoms in the elastic network models can be very bad for the results.
+
+        Notes:
+            * This function will be moved to the molecule manipulation package later
+
+        
+        Args:
+            distance (float): The distance cutoff user wishes to defined as a clash radius (default:0.77; max bond length)
+        
+        Returns:
+            clashes (int)   : Number of clashes present according to the set cutoff.
+        """
         from scipy.spatial import KDTree
         all_atoms=[i for i in self.get_atoms()]
         T=KDTree([i.get_location() for i in all_atoms])
