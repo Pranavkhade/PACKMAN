47b96909e4d9c1d76648d84dd60eee2d5e736e90
Pranav Khade <pranav@Pranavs-MacBook-Pro.local>
2020-03-27 18:08:20 -0500

    1.1.3 (Minor fixes) commit1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ SCRIPTS=['packman=packman.bin.PACKMAN:main']
 
 
 setup(name='py-packman',
-      version='1.1.2',
+      version='1.1.3',
       description='A software package for molecular PACKing and Motion ANalysis (PACKMAN)',
       url='https://github.com/Pranavkhade/PACKMAN',
       author='Pranav Khade',
